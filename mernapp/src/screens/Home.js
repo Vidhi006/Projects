d00bb10500113915d7cc2960939f63ac432fdb5c
Vidhi006 <joshividhi787@gmail.com>
2025-08-07 01:19:38 +0530

    commit

---
+++ mernapp/src/screens/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
+export default function Home() {
+  return (
+    <div>
+        <div>
+            <Navbar/>
+        </div>
+        <div>body</div>
+        <div>
+            <Footer/>
+        </div>
+    </div>
+  )
+}
