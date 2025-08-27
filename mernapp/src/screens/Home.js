cc71722c1bc7d1da08f7d0c8dc67ab5aeacc3ced
Vidhi006 <joshividhi787@gmail.com>
2025-08-27 14:46:26 +0530

    changes

--- mernapp/src/screens/Home.js
+++ mernapp/src/screens/Home.js
@@ -4,13 +4,20 @@ import Navbar from '../components/Navbar'
 export default function Home() {
   return (
     <div>
-        <div>
-            <Navbar/>
+      <div>
+        <Navbar />
+      </div>
+      <div><div className="card mt-3" style={{ "width": "18rem" , "maxHeight":"360px"}}>
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <div className=''></div>
         </div>
-        <div>body</div>
-        <div>
-            <Footer/>
-        </div>
+      </div></div>
+      <div>
+        <Footer />
+      </div>
     </div>
   )
 }
